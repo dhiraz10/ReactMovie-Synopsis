70f4f8bc47311cac77d7de464ed7fd1e50c03219
Dhiraj chauhan <dhiraj9843775937@gmail.com>
2021-01-20 19:31:38 +0545

    first commit

---
+++ project/src/components/Footer/index.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import {animateScroll as scroll} from 'react-scroll';
+import { FooterContainer,FooterWrap,FooterLinksContainer,
+    FooterLinksWrapper,SocialIcons,
+    SocialMedia,SocialMediaWrap,SocialLogo,WebsiteRights,SocialIconsLink
+} from "./style";
+import {FaFacebook, FaInstagram} from  "react-icons/fa";
+function Footer() {
+    const toggleHome =() =>{
+        scroll.scrollToTop();
+      };
+    return (
+       <FooterContainer>
+           <FooterWrap>
+               <FooterLinksContainer>
+               <FooterLinksWrapper>
+                          <p>It's a simple project that is created on React JS</p>
+               </FooterLinksWrapper>
+               </FooterLinksContainer>
+               <SocialMedia>
+                   <SocialMediaWrap>
+                       <SocialLogo to="/" onClick={toggleHome}>
+                                Movies-Synopsis
+                       </SocialLogo>
+                       <WebsiteRights>  Movies-Synopsis &#169;  {new Date().getFullYear()} &nbsp; All rights reserved .</WebsiteRights>
+                       <SocialIcons>
+                           <SocialIconsLink href = "//www.facebook.com/dhiraj.chauhan.5268" target="_blank" aria-label="Facebook">
+                           
+                               <FaFacebook/>
+                               Facebook
+                           </SocialIconsLink>
+                           <SocialIconsLink href = "//www.instagram.com/dhiraz1/" target="_blank" aria-label="Instagram">
+                      
+                               <FaInstagram/>
+                               Instagram
+                           </SocialIconsLink>
+                       </SocialIcons>
+                   </SocialMediaWrap>
+               </SocialMedia>
+           </FooterWrap>
+       </FooterContainer>
+    )
+}
+
+export default Footer
---
+++ project/src/components/Homepage/SinglePage/style.js
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+export const SingleContainer = styled.div `
+color:#fff;
+height: 100%;
+
+.wapperStyle{
+    margin-top: 80px;
+    padding: 20px;
+}
+.cardstyle{
+    background: rgba( 255, 255, 255, 0.25 );
+box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );
+backdrop-filter: blur( 1.0px );
+-webkit-backdrop-filter: blur( 1.0px );
+border-radius: 10px;
+justify-content: center;
+align-items: center;
+width:300px;
+/* height:500px; */
+
+}
+`
+export const Img = styled.img `
+margin:10px;
+padding:10px;
+height: 400px;
+width: 300px;
+
+`
+export const H2 = styled.h1 `
+color:rgba( 248,231,28 );
+text-transform: uppercase ;
+
+`
+export const Paragraph = styled.p `
+margin: 10 px;
+font-family: "Poppins", sans-serif;
+
+
+`
+export const Overview = styled.h3 `
+margin: 5px;
+color:rgba( 255, 255, 255, 0.70 );
+
+
+`
